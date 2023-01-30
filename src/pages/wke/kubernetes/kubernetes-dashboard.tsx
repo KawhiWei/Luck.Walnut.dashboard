8f77cbc193eae1e6683eb3376ee8ce63026125db
王泽威 <1585955375@qq.com>
2023-01-30 19:02:46 +0800

    feat : 优化k8s看板样式

--- src/pages/wke/kubernetes/kubernetes-dashboard.tsx
+++ src/pages/wke/kubernetes/kubernetes-dashboard.tsx
@@ -1,4 +1,5 @@
-import { Area, Pie, Radar } from "@ant-design/charts";
+import { AntDesignChartsRadarDto, KubernetesClusterDashboardDto } from "@/domain/kubernetes/clusters/kubernetes-cluster-dto";
+import { Area, G2, Liquid, Pie, Radar, RingProgress } from "@ant-design/charts";
 import {
   Card,
   Col,
@@ -13,12 +14,13 @@ import {
 } from "antd";
 import { useEffect, useState } from "react";
 
-import { AntDesignChartsRadarDto } from "@/domain/kubernetes/clusters/kubernetes-cluster-dto";
 import { IClusterService } from "@/domain/kubernetes/clusters/icluster-service";
 import { IocTypes } from "@/shared/config/ioc-types";
 import useHookProvider from "@/shared/customHooks/ioc-hook-provider";
 
 const KubernetesDashboard = (props: any) => {
+  const G = G2.getEngine('canvas');
+
   const _clusterService: IClusterService = useHookProvider(
     IocTypes.ClusterService
   );
@@ -29,6 +31,13 @@ const KubernetesDashboard = (props: any) => {
   const [radarData, setRadarData] = useState<Array<AntDesignChartsRadarDto>>(
     []
   );
+  const [cpuUsagePercent, setCpuUsagePercent] = useState<number>(0);
+  const [memoryUsagePercent, setMemoryUsagePercent] = useState<number>(0);
+  const [nodeReadyPercent, setNodeReadyPercent] = useState<number>(0);
+  const [podUsagePercent, setPodUsagePercent] = useState<number>(0);
+  const [kubernetesClusterDashboardData, setKubernetesClusterDashboardData] = useState<KubernetesClusterDashboardDto>();
+
+
   const data = [
     {
       type: "分类一",
@@ -55,27 +64,113 @@ const KubernetesDashboard = (props: any) => {
       value: 5,
     },
   ];
-  const config = {
+  const overviewConfig = {
     appendPadding: 10,
-    height: 200,
     data: radarData.map((d) => ({ ...d, star: Math.sqrt(d.count) })),
     angleField: "count",
     colorField: "name",
-    radius: 0.75,
+    radius: 0.5,
     label: {
-      type: "spider",
-      labelHeight: 28,
+      type: 'spider',
+      labelHeight: 40,
+      formatter: (data: any, mappingData: any) => {
+        const group = new G.Group({});
+        group.addShape({
+          type: 'circle',
+          attrs: {
+            x: 0,
+            y: 0,
+            width: 40,
+            height: 50,
+            r: 5,
+            fill: mappingData.color,
+          },
+        });
+        group.addShape({
+          type: 'text',
+          attrs: {
+            x: 10,
+            y: 8,
+            text: `${data.name}`,
+            fill: mappingData.color,
+          },
+        });
+        group.addShape({
+          type: 'text',
+          attrs: {
+            x: 0,
+            y: 25,
+            text: `${data.count}个 ${(data.percent * 100).toFixed(2)}%`,
+            fill: 'rgba(0, 0, 0, 0.65)',
+            fontWeight: 700,
+          },
+        });
+        return group;
+      },
     },
     interactions: [
       {
-        type: "element-selected",
+        type: 'element-selected',
       },
       {
-        type: "element-active",
+        type: 'element-active',
       },
     ],
   };
 
+  const cpuUsagePercentConfig = {
+    percent: cpuUsagePercent,
+    height: 150,
+    shape: 'diamond',
+    outline: {
+      border: 2,
+      distance: 4,
+    },
+    wave: {
+      length: 128,
+    },
+  };
+
+  const memoryUsagePercentConfig = {
+    percent: memoryUsagePercent,
+    height: 150,
+    shape: 'diamond',
+    outline: {
+      border: 2,
+      distance: 4,
+    },
+    wave: {
+      length: 128,
+    },
+
+  };
+
+  const nodeReadyPercentConfig = {
+    percent: nodeReadyPercent,
+    height: 150,
+    shape: 'diamond',
+    outline: {
+      border: 2,
+      distance: 4,
+    },
+    wave: {
+      length: 128,
+    },
+
+  };
+  const podUsagePercentConfig = {
+    percent: podUsagePercent,
+    height: 150,
+    shape: 'diamond',
+    outline: {
+      border: 2,
+      distance: 4,
+    },
+    wave: {
+      length: 128,
+    },
+
+  };
   /**
    * 页面初始化事件
    */
@@ -144,46 +239,109 @@ const KubernetesDashboard = (props: any) => {
             count: rep.result.nodes.length,
           },
         ];
+        setKubernetesClusterDashboardData(rep.result)
         setRadarData(data);
+        setCpuUsagePercent(rep.result.clusterTotalCpuUsage / rep.result.clusterTotalCpuCapacity);
+        setMemoryUsagePercent(rep.result.clusterTotalMemoryUsage / rep.result.clusterTotalMemoryCapacity);
+        setNodeReadyPercent(rep.result.nodes.length / rep.result.nodes.length);
+        setPodUsagePercent(rep.result.clusterTotalPodUsage / rep.result.clusterTotalPodCapacity)
+
       })
-      .finally(() => {});
+      .finally(() => { });
   };
 
   return (
     <div>
       <Spin spinning={globalLoading}>
-        <Row gutter={[16, 16]}>
+        <Row gutter={[10, 10]}>
+          <Col span="16">
+            <Row gutter={[10, 10]}>
+              <Card style={{ width: '100%' }}>
+                <Row style={{ fontSize: 20, color: '#bdc3c7' }}>
+                  Luck WKE  kubernetes Dashboard
+                </Row >
+                <Row style={{ color: '#bdc3c7', paddingTop: 10 }}>
+                  Luck Walnut is an PaaS Platform, An Cloud-Native Application Platform for Kubernetes.
+                </Row>
+                <Row style={{ paddingTop: 10 }}>
+                  <Col span="12">
+                    集群列表：
+                    <Select
+                      allowClear={true}
+                      value={selectValue}
+                      style={{ width: 180 }}
+                      onChange={selectHandleChange}
+                    >
+                      {selectData.map((item) => {
+                        return (
+                          <Select.Option value={item.id}>
+                            {item.name}
+                          </Select.Option>
+                        );
+                      })}
+                    </Select>
+                  </Col>
+                </Row>
+              </Card>
+            </Row>
+            <Row gutter={[10, 10]} style={{ paddingTop: 10 }}>
+              <Col span="4">
+                <Card title="Node Information">
+                  <Liquid {...nodeReadyPercentConfig} />
+                </Card>
+              </Col>
+              <Col span="4">
+                <Card title="Cpu Usage">
+                  <Liquid {...cpuUsagePercentConfig} />
+                </Card>
+              </Col>
+              <Col span="4">
+                <Card title="Memory Usage">
+                  <Liquid {...memoryUsagePercentConfig} />
+                </Card>
+              </Col>
+              <Col span="4">
+
+                <Card title="Pod Usage">
+                  <Liquid {...podUsagePercentConfig} />
+                </Card>
+
+              </Col>
+              <Col span="4">
+                <Card title="Cpu Usage">
+                  <RingProgress {...cpuUsagePercentConfig} />
+                </Card>
+              </Col>
+              <Col span="4">
+                <Card title="Cpu Usage">
+                  <RingProgress {...cpuUsagePercentConfig} />
+                </Card>
+              </Col>
+            </Row>
+          </Col>
           <Col span="8">
-            <Card title="概览">
-              <Row gutter={[16, 16]}>
-                <Col span="12">
-                  集群列表：
-                  <Select
-                    allowClear={true}
-                    value={selectValue}
-                    style={{ width: 180 }}
-                    onChange={selectHandleChange}
-                  >
-                    {selectData.map((item) => {
-                      return (
-                        <Select.Option value={item.id}>
-                          {item.name}
-                        </Select.Option>
-                      );
-                    })}
-                  </Select>
-                </Col>
-              </Row>
+            <Card title="Overview">
+              <Pie {...overviewConfig} />
             </Card>
           </Col>
         </Row>
-        <Row gutter={[16, 16]}>
+        {/* <Row gutter={[10, 10]} style={{ paddingTop: 10 }}>
           <Col span="4">
-            <Card title="概览">
-              <Pie {...config} />
+            <Card title="Cpu Usage">
+              <RingProgress {...cpuUsagePercentConfig} />
             </Card>
           </Col>
-        </Row>
+          <Col span="4">
+            <Card title="Memory Usage">
+              <RingProgress {...memoryUsagePercentConfig} />
+            </Card>
+          </Col>
+          <Col span="4">
+            <Card title="Pod Usage">
+              <Pie {...overviewConfig} />
+            </Card>
+          </Col>
+        </Row> */}
       </Spin>
     </div>
   );
