c3d5cd9dd5105ec03865920c28e560c009f9525c
王泽威 <1585955375@qq.com>
2023-02-23 18:58:29 +0800

    feat : 优化代码

--- src/domain/deployment-configurations/deployment-configuration-dto.ts
+++ src/domain/deployment-configurations/deployment-configuration-dto.ts
@@ -1,5 +1,8 @@
 import { IEntity } from "@/shared/entity";
 
+/**
+ * 应用部署配置Dto
+ */
 export interface IDeploymentConfigurationBaseDto {
     environmentName: string;
     applicationRuntimeType: number;
@@ -13,8 +16,61 @@ export interface IDeploymentConfigurationBaseDto {
     imagePullSecretId: string;
 }
 
-
-
+/**
+ * 输出Dto
+ */
 export interface IDeploymentConfigurationOutputDto extends IDeploymentConfigurationBaseDto, IEntity<string> {
+    applicationContainers: IApplicationContainerOutputDto[];
+}
+
+/**
+ * 容器配置基础Dto
+ */
+export interface IApplicationContainerBase {
+    containerName: string;
+    restartPolicy: string;
+    isInitContainer: boolean;
+    imagePullPolicy: string;
+    image: string;
+    readinessProbe: INessProbe;
+    liveNessProbe: INessProbe;
+    cpuContainerResourceQuantity: IContainerResourceQuantity;
+    memoryContainerResourceQuantity: IContainerResourceQuantity;
+    environments: Map<Object, Object>;
+    containerPortConfigurations: IContainerPortConfiguration[];
+}
+
+/**
+ * 端口配置
+ */
+export interface IContainerPortConfiguration {
+    name: string;
+    containerPort: number;
+    protocol: string;
+}
+/**
+ * 容器资源配置
+ */
+export interface IContainerResourceQuantity {
+    limit: string;
+    request: string;
+}
+
+/**
+ * 健康检查端口配置
+ */
+export interface INessProbe {
+    scheme: string;
+    path: string;
+    port: number;
+    initialDelaySeconds: number;
+    periodSeconds: number;
+}
+
+
+/**
+ * 容器输出Dto
+ */
+export interface IApplicationContainerOutputDto extends IApplicationContainerBase, IEntity<string> {
 
 }
