e94956622aad2c29ad47727f56209052d23b050b
kawhi <1585955375@qq.com>
2023-09-14 14:54:12 +0800

    feat ：测试使用rspack

---
+++ rspack.config.js
@@ -0,0 +1,25 @@
+const CopyPlugin = require('copy-webpack-plugin');
+
+/**
+ *  @type {import('@rspack/cli').Configuration}
+ */
+const config = {
+  entry: {
+    main: './src/index.jsx', // 配置项目入口文件
+  },
+  builtins: {
+    html: [
+      {
+        template: './index.html', // 对齐 CRA 生成index.html
+      },
+    ],
+    copy: {
+      patterns: [
+        {
+          from: 'public',
+        },
+      ],
+    },
+  },
+};
+module.exports = config;
